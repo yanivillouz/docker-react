e1b0dc12fe364e7900296dabab8d6dd5e4bb87df
Yaniv Illouz <yanivi@checkpoint.com>
2019-09-19 13:52:19 +0300

    fix2

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload - changed by yaniv.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
 	<p className="App-intro">
-	  I was changed on the Feature branch
+	  Hi there
 	  </p>
     </div>
-    );
-  }
+  );
 }
 
 export default App;
